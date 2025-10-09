ca0ef5a828e58956691a6e383ce6235d357110b8
VaC1av-111 <real.vaclav@gmail.com>
2025-10-09 08:49:41 +0000

    9.10.25

---
+++ cviceni2.py
@@ -0,0 +1,67 @@
+"""""
+if __name__ == "__main__":
+    seznam = [100, 5, 3, 21]
+
+    seznam[2] *= 2
+
+    seznam.append(55)
+
+    seznam.sort()
+
+    seznam.reverse()
+
+    print(seznam)
+
+
+def vrat_treti_prvek(seznam):
+    if len(seznam) >= 3:
+        return seznam[2]
+    else:
+        return None
+
+def prumer(cisla):
+    if not cisla:
+        return 0.0
+    return sum(cisla) / len(cisla)
+
+if __name__ == "__main__":
+    cisla = [1, 2, 3, 4, 5]
+    print(f"Průměr je: {prumer(cisla)}")  # vypise 3.0
+
+    if __name__ == "__main__":
+        #prace_se_seznamem()
+        x=[1, 2]
+        print(vrat_treti_prvek(x)) # none
+        
+        
+        y=[12,34,56,78]
+        print(vrat_treti_prvek(y)) # 56
+"""
+     
+
+def prumer(znamky):
+    if not znamky:
+        return 0.0
+    return sum(znamky) / len(znamky)
+
+def naformatuj_text(zak):
+    return f"Student: {zak['jmeno']} {zak['prijmeni']}, Vek: {zak['rocnik']}, Obor: {zak['obor']}, Prumer: {prumer(zak['znamky'])}"
+
+
+if __name__ == "__main__":
+    
+    student = {
+        "jmeno": "Jan",
+        "prijmeni": "Novak",
+        "rocnik": 22,
+        "znamky": [1, 2, 3, 1, 2, 1]
+        }
+
+    student["obor"] = "Informatika"
+    student["rocnik"] += 1
+    print(f"student['znamky'][2]")
+    print(naformatuj_text(student))  # vypise "Student Jan Novak, Vek 23, Obor: Informatika, Prumer: 1.6666666666666667"""
+
+
+
+
