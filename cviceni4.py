38a22f2604c0ac2103ff6d04dd5553afb49d4e8f
VaC1av-111 <real.vaclav@gmail.com>
2025-10-23 06:32:35 +0000

    seznamy

---
+++ cviceni4.py
@@ -0,0 +1,28 @@
+fronta = []
+fronta.append(5)
+fronta.append(10)
+fronta.append(15)
+
+print (fronta.pop(0))
+
+fronta.append(3)
+
+print (fronta.pop(0))
+print (fronta.pop(0))
+print (fronta.pop(0))
+
+
+
+zasobnik = []
+zasobnik.append(5)
+zasobnik.append(10)
+zasobnik.append(15)
+
+print (zasobnik.pop())
+
+zasobnik.append(3)
+
+print (zasobnik.pop(0))
+print (zasobnik.pop(0))
+print (zasobnik.pop(0))
+
